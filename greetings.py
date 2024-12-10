6b3eabca3e413f8bce4e3fa3a2a8f3b286a8e1be
BillyTheHero <you@example.com>
2024-12-10 23:23:34 +0800

    first commit

---
+++ greetings.py
@@ -0,0 +1,17 @@
+username = "Зорин Родион Сергеевич"
+print("Имя пользователя", username)
+
+title = "Список покупок"
+print("Заголовок", title)
+
+content = "Яйца, молоко, хлеб"
+print("Наполнение", content)
+
+status = "Активна"
+print("Статус", status)
+
+created_date = "10.12.2024"
+print("Дата создания", created_date)
+
+issue_date = "11.12.2024"
+print("Дата дедлайна",issue_date)
